012694e937942476faba7bc19bcc7e8a2f34bcd9
Jonathon Mullins <jonathonm93@gmail.com>
2016-11-16 11:37:31 -0700

    updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ints:
+            int Char_HP_Current = 20;
+            int Char_HP_Full = 100;
+
+            int Char_EXP_Current = 10;
+            int Char_EXP_Full = 100;
+            int Char_Level_Current = 1;
+
+            //string:
+            string Char_Name = "Class inc.";
+            Console.WriteLine("");
+            Console.SetCursorPosition(5, 1);
+            Console.WriteLine("Name: {0}", Char_Name);
+            Console.SetCursorPosition(35, 1);
+            Console.WriteLine("HP: {0}/{1}", Char_HP_Current, Char_HP_Full);
+            Console.SetCursorPosition(60,1);
+            Console.WriteLine("EXP: {0}/{1}, Char_EXP_Current, Char_EXP_Full");
+            Console.ReadLine();
         }
     }
 }
